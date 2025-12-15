a695859bbcb1ce1333d8867badaf00b94b2a3025
Donatien Wallaert <donatien@MacBook-Air-de-Donatien.local>
2025-12-15 14:53:31 +0100

    UP my solution

--- numpy_questions.py
+++ numpy_questions.py
@@ -1,4 +1,4 @@
-"""Assignment - using numpy and making a PR. 
+"""Assignment - using numpy and making a PR.
 
 The goals of this assignment are:
     * Use numpy in practice with two easy exercises.
@@ -41,6 +41,10 @@ def max_index(X):
     j = 0
 
     # TODO
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise ValueError("Input must be a 2D numpy array.")
+
+    i, j = np.unravel_index(np.argmax(X, axis=None), X.shape)
 
     return i, j
 
@@ -64,4 +68,11 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    return 0.
+    if n_terms == 0:
+        return 2
+    product = 1.0
+    n = 1
+    while n <= n_terms:
+        product *= 4 * (n ** 2) / (4 * (n ** 2) - 1)
+        n += 1
+    return 2 * product
